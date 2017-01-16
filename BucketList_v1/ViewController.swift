ab071b9417a6c46c220d8770ca4e797679649263
Calvin Nguyen <imcalv@gmail.com>
2017-01-16 13:52:18 -0800

    Initial Commit

---
+++ BucketList_v1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BucketList_v1
+//
+//  Created by Calvin Nguyen on 1/16/17.
+//  Copyright © 2017 Calvin Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
